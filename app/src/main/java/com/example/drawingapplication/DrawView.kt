6e7cc5d8245abc8b8175ba9e32ddba2c3c65ac82
Landon Vong <lando.vong@gmail.com>
2024-11-25 23:19:41 -0700

    Added DrawView because code needs it

---
+++ app/src/main/java/com/example/drawingapplication/DrawView.kt
@@ -0,0 +1,13 @@
+package com.example.drawingapplication
+
+import android.content.Context
+import android.graphics.Canvas
+import android.os.Build
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.annotation.RequiresApi
+
+class DrawView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+}
